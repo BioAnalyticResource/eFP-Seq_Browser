c47240c8edb7082c364ed5f9cb0a3e72a9a6b769
Alexander Sullivan <alexander.j.sullivan@hotmail.com>
2016-11-15 22:28:00 -0500

    Changed XML template
    Split XML template into three seperate sections; base, adding and end

--- Submission_page/XMLgenerator-v2.js
+++ Submission_page/XMLgenerator-v2.js
@@ -1,16 +1,10 @@
-$(function (){
-  $("#CloneForm").click(function (){
-    $("body").append($("#Entries:first").clone(true));
-  });
-});
-
 $(function () {
   $('#SubmitButton').click(update);
 });
 
 var template = [
   '<?xml version="1.0" encoding="UTF-8" standalone="no"?>',
-  '<!DOCTYPE rnaseq_experiments SYSTEM "bamdata_amazon_links.dtd"',
+  '<!DOCTYPE rnaseq_experiments SYSTEM "bamdata_amazon_links.dtd">',
   '\t<rnaseq_experiments>',
   '\t\t<bam_file desc=\"<?channeldescription?>\" record_number=\"<?channelrecordnumber?>\" hex_color=\"<?channelhexcolor?>\" bam_link=\"<?channelbamlink?>\" total_reads_mapped=\"<?channeltotalreadsmapped?>\" publication_link=\"<?channelpublicationlink?>\" svg_subunit=\"<?channeltissue?>\" svgname="<?channelsvgname?>\" title=\"<?channeltitle?>\" publication_url=\"<?channelpublicationlink?>\">',
   '\t\t\t<controls>',
@@ -23,6 +17,26 @@ var template = [
   '\t</rnaseq_experiments>'
 ].join('\r\n');
 
+var base = [
+  '<?xml version="1.0" encoding="UTF-8" standalone="no"?>',
+  '<!DOCTYPE rnaseq_experiments SYSTEM "bamdata_amazon_links.dtd"',
+  '\t<rnaseq_experiments>',
+].join('\r\n');
+
+var end = [
+  '\t</rnaseq_experiments>'
+].join('\r\n');
+
+var adding = [
+  '\t\t<bam_file desc=\"<?channeldescription?>\" record_number=\"<?channelrecordnumber?>\" hex_color=\"<?channelhexcolor?>\" bam_link=\"<?channelbamlink?>\" total_reads_mapped=\"<?channeltotalreadsmapped?>\" publication_link=\"<?channelpublicationlink?>\" svg_subunit=\"<?channeltissue?>\" svgname="<?channelsvgname?>\" title=\"<?channeltitle?>\" publication_url=\"<?channelpublicationlink?>\">',
+  '\t\t\t<controls>',
+  '\t\t\t\t<bam_exp><?channelcontrols?></bam_exp>',
+  '\t\t\t</controls>',
+  '\t\t\t<groupwidth>',
+  '\t\t\t\t<bam_exp><?channelgroupwidth?></bam_exp>',
+  '\t\t\t</groupwidth>',
+  '\t\t</bam_file>',
+].join('\r\n');
 
 function update() {
   var variables = {
@@ -40,15 +54,24 @@ function update() {
     'channelgroupwidth': $('#channelgroupwidth').val()
   };
 
-  var newXml = template.replace(/<\?(\w+)\?>/g,
+  var newXml = adding.replace(/<\?(\w+)\?>/g,
     function(match, name) {
       return variables[name];
     });
 
+  var finalXML = base + '\n' + newXml + '\n' + end; 
 
-  $('#ResultXml').val(newXml);
+  $('#ResultXml').val(finalXML);
   $('#DownloadLink')
-    .attr('href', 'data:text/xml;base64,' + btoa(newXml))
+    .attr('href', 'data:text/xml;base64,' + btoa(finalXML))
     .attr('download', 'bamdata.xml');
   $('#generated').show();
 }
+
+onClick = 0
+
+$(function (){
+  $("#CloneForm").click(function (){
+    $("body").append($("#Entries:first").clone(true));
+  });
+});
