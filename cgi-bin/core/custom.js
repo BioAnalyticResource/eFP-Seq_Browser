c062147f89e3d1a72b245118dcab5479bc6c7ce2
Alexander Sullivan <ASully@users.noreply.github.com>
2017-11-17 02:19:31 -0500

    Added “Click for More Details” to description
    Added “Click for More Details” to description
    
    Added an option to “Click for More Details” into the main page table description panel which contains controls and species information. This was coded into the custom.js
    
    Also moved the “SVG Colouring Mode” section up a tiny bit (changes made in index.html)
    
    Known bugs:
    • UI bugs out when resolution is less than 598pixels wide
    • After generating new data, scrolling up doesn’t work unless users scroll down first
    • Does not work on IE, unknown reasons
    • Help page still not completed
    • Controls are not in either of the Excel exporters
    • When selecting more than two datasets to add from existing data to
    generate new data, it does not add to the generate new data modal
    • Google Drive files in Klepikova data are not precache properly
    • Private datasets are not loading in order all the time

--- cgi-bin/core/custom.js
+++ cgi-bin/core/custom.js
@@ -1093,7 +1093,7 @@ function populate_table(status) {
         // Append abs/rel RPKM
         append_str += '<td id="' + experimentno + '_rpkm' + '" style="font-size: 10px; width: 50px; ">-9999</td>';
         // Append the details <td>
-        append_str += '<td style="width: 200px; font-size: 12px;">' + description + '<br/>' + '<a href="' + url + '" target="blank">' + 'NCBI SRA for ' + experimentno + '</a>; <a href="' + publicationid + '" target="blank">PubLink</a>' + '<br/><a href="javascript:(function(){$(\'#' + url.substring(44) + '\').toggle();})()"></a><div id="' + url.substring(44) + '" >Total reads = ' + numberofreads + '.<br/>Controls: ' + links + '.<br/>Species: ' + species + '</div></td>\n';
+        append_str += '<td style="width: 200px; font-size: 12px;">' + description + '<br/>' + '<a href="' + url + '" target="blank">' + 'NCBI SRA for ' + experimentno + '</a>; <a href="' + publicationid + '" target="blank">PubLink</a>' + '.<br/>' + 'Total reads = ' + numberofreads + '<br/><a href="javascript:(function(){$(\'#' + url.substring(44) + '\').toggle();})()">Click for More Details</a><div id="' + url.substring(44) + '" style="display:none">Controls: ' + links + '.<br/>Species: ' + species + '</div></td>\n';
         append_str += '</tr>';
 
         // Append the <tr> to the table
