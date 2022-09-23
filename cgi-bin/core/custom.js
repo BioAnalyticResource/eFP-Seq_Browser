dfb3096f2419f8296b761a77e38ef2a359e5bdb9
Alexander Sullivan <alexander.j.sullivan@hotmail.com>
2022-09-22 21:45:11 -0400

    Manifest should match BAR home page

--- cgi-bin/core/custom.js
+++ cgi-bin/core/custom.js
@@ -4,7 +4,7 @@
 //
 //=============================================================================
 /** Current version of eFP-Seq Browser with the following format: [v-version][version number: #.#.#][-][p-public OR d-dev][year - 4 digits][month - 2 digits][day - 2 digits] */
-const version = "v1.3.14-p20220825";
+const version = "v1.3.14-p20220922";
 
 /** Selected RPKM mode */
 let colouring_mode = "abs";
